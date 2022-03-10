0a43da260c0eda70c3e8000356d60146fe4615c2
thuitaf <faith.thuita@moringaschool.com>
2022-03-10 16:12:16 +0300

    commit tree 0891afccbb161125215740c4f32c1d7e01ac53d8

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record){
+
+    const result = record.find(record => record.result === "W" );
+
+    return !!result ? result.year : undefined;
+
+    
+
+}
